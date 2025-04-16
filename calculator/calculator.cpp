475cbe4a1762ad00aa035ceda3f7e4cda145007b
Arthur Kurbanov <arthurkurbanov@MacBook-Pro-Arthur.local>
2025-04-16 23:12:45 +0300

    Message

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,82 @@
-// В этом файле определения функций.
-// За основу возьмите решение предыдущей задачи.
+#include<iostream>
+#include<cmath>
+#include"calculator.h"
+
+bool ReadNumber(Number& result){
+    if(!(std::cin >> result)){
+        std::cerr << "Error: Numeric operand expected" << std::endl;
+    }
+    return std::cin.good();
+}
+
+bool RunCalculatorCycle(){
+    Number result = 0;
+    if (ReadNumber(result)) {
+        std::string token;
+        bool stop = false;
+        Number memory = 0;
+        bool memory_initialized = false;
+        while (!stop){
+            std::cin >> token;
+            if (Number input; token == "q") {
+                return true;
+            } else if (token == "+"){
+                if(ReadNumber(input)){  
+                    result += input;                 
+                } else {
+                    stop = true;
+                }
+            } else if (token == "-"){
+                if(ReadNumber(input)){  
+                    result -= input;                 
+                } else {
+                    stop = true;
+                }
+            } else if (token == "*"){
+                if(ReadNumber(input)){  
+                    result *= input;                 
+                } else {
+                    stop = true;
+                }
+            } else if (token == "/"){
+                if(ReadNumber(input)){  
+                    result /= input;                 
+                } else {
+                    stop = true;
+                }
+            } else if (token == "**"){
+                if(ReadNumber(input)){                   
+                    result = pow(result,input);                 
+                } else {
+                    stop = true;
+                }
+            } else if(token == ":"){
+                if(ReadNumber(input)){                   
+                    result = input;                 
+                } else {
+                    stop = true;
+                }
+            } else if(token == "s"){
+                memory = result;
+                memory_initialized = true;
+            } else if(token == "l"){
+                if(memory_initialized){                   
+                    result = memory;                 
+                } else {
+                    std::cerr << "Error: Memory is empty" << std::endl;
+                    stop = true;
+                }
+            } else if(token == "c"){
+                result = 0;
+            } else if(token == "="){
+                std::cout << result << std::endl;
+            } else {
+                std::cerr << "Error: Unknown token " << token << std::endl;;
+                stop = true;
+            }           
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,1 +1,9 @@
-// В этом файле только функция main и #include-директивы.
+#include"calculator.h"
+
+int main(){
+    if(RunCalculatorCycle()){
+        return 0;
+    } else{
+        return -1;
+    }
+}
